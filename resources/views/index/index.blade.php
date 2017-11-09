6a184fbf168ba0d0fd0f5b5b608a95ebcc744f8e
José Angel Lujan Villaseñor <jose.lujan@lemonthree.mx>
2017-11-08 21:53:42 -0700

    hexagons area, footer, fonts added

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -10,4 +10,100 @@
             <img class="img-fluid vertical-align absolute-centered" src="{{ asset('img/index/logos/border.png') }}" alt="Border Opportunities">
         </div>
     </div>
+    <div class="hexagons" id="hexagons">
+        <div class="hexagons-container spacing">
+            <ul id="hexGrid">
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm7.staticflickr.com/6139/5986939269_10721b8017.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+                <li class="hex">
+                    <div class="hexIn">
+                        <a class="hexLink" href="#">
+                            <img src="https://farm4.staticflickr.com/3165/5733278274_2626612c70.jpg" alt="" />
+                            <h1>This is a title</h1>
+                            <p>Some sample text about the article this hexagon leads to</p>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 @stop
